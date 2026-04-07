fc4809052b79d51671040d79f4b4871da56aa90e
larataweel79 <larataweel97@gmail.com>
2026-04-07 21:40:15 +0300

    Add files via upload

---
+++ AuthService.ts
@@ -0,0 +1,23 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "./firebase";
+
+export const signup = async (email: string, password: string) => {
+  const response = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+  return response.user;
+};
+
+export const login = async (email: string, password: string) => {
+  const response = await signInWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+  return response.user;
+};
