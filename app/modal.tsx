242a2bdaef94cd7587e74439afadc65f82747f7b
karamabugosh <151734659+karamabugosh@users.noreply.github.com>
2026-05-13 03:45:37 +0300

    this is a test

--- app/modal.tsx
+++ app/modal.tsx
@@ -3,7 +3,8 @@ import { StyleSheet } from 'react-native';
 
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-
+ 
+//this is a test
 export default function ModalScreen() {
   return (
     <ThemedView style={styles.container}>
